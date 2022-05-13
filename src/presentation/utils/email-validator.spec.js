9ddd46faf320f2f9fb2ebcf265351b1122bf78e5
Juan Cassiano <juancassiano@hotmail.com>
2022-05-13 08:48:06 -0300

    ensure email validator returns false if validator returns false

--- src/presentation/utils/email-validator.spec.js
+++ src/presentation/utils/email-validator.spec.js
@@ -1,13 +1,21 @@
+const validator = require("validator");
 class EmailValidator {
   isValid(email) {
-    return true;
+    return validator.isEmail(email);
   }
 }
 
 describe("Email Validator", () => {
   test("Should return true if validator returns true", () => {
+    validator.isEmailValid = false;
     const sut = new EmailValidator();
     const isEmailValid = sut.isValid("valid_email@mail.com");
-    expect(isEmailValid).toBe(true);
+    expect(isEmailValid).toBe(false);
+  });
+
+  test("Should return false if validator returns false", () => {
+    const sut = new EmailValidator();
+    const isEmailValid = sut.isValid("invalid_email@mail.com");
+    expect(isEmailValid).toBe(false);
   });
 });
