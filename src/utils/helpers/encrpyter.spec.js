18f21931f27fcd7741b9387d31f4aea3ce1d28bd
Juan Cassiano <juancassiano@hotmail.com>
2022-05-24 09:05:16 -0300

    move sut creation to a factory helper method

--- src/utils/helpers/encrpyter.spec.js
+++ src/utils/helpers/encrpyter.spec.js
@@ -8,9 +8,13 @@ class Encrypter {
   }
 }
 
+const makeSut = () => {
+  return new Encrypter();
+};
+
 describe("Encrypter", () => {
   test("Should return true if bcrypt returns true", async () => {
-    const sut = new Encrypter();
+    const sut = makeSut();
 
     const isValid = await sut.compare("any_value", "hashed_value");
 
@@ -18,7 +22,8 @@ describe("Encrypter", () => {
   });
 
   test("Should return false if bcrypt returns false", async () => {
-    const sut = new Encrypter();
+    const sut = makeSut();
+
     bcrypt.isValid = false;
 
     const isValid = await sut.compare("any_value", "hashed_value");
@@ -27,7 +32,7 @@ describe("Encrypter", () => {
   });
 
   test("Should call Brypt with correct values", async () => {
-    const sut = new Encrypter();
+    const sut = makeSut();
 
     const isValid = await sut.compare("any_value", "hashed_value");
 
