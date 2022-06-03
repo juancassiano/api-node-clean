829c4bac59c8b00ca2ae6475df0614583bed5e62
Juan Cassiano <juancassiano@hotmail.com>
2022-06-03 13:01:25 -0300

    enable cors

--- src/main/config/setup.test.js
+++ src/main/config/setup.test.js
@@ -9,4 +9,14 @@ describe("App Setup", () => {
     const res = await request(app).get("/test_x_powered_by");
     expect(res.header["x-powered-by"]).toBeUndefined();
   });
+
+  test("Should enable CORS", async () => {
+    app.get("/test_cors", (req, res) => {
+      res.send("");
+    });
+    const res = await request(app).get("/test_cors");
+    expect(res.header["access-control-allow-origin"]).toBe("*");
+    expect(res.header["access-control-allow-methods"]).toBe("*");
+    expect(res.header["access-control-allow-headers"]).toBe("*");
+  });
 });
