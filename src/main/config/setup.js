8842467bd06029ebf0aa37e25d641cb7b13fccec
Juan Cassiano <juancassiano@hotmail.com>
2022-06-03 13:12:17 -0300

    ensure requests parse body as json

--- src/main/config/setup.js
+++ src/main/config/setup.js
@@ -1,7 +1,10 @@
 const cors = require("../middlewares/cors");
+const express = require("express");
+const jsonParser = require("../middlewares/json-parser");
 
 module.exports = (app) => {
   app.disable("x-powered-by");
 
   app.use(cors);
+  app.use(jsonParser);
 };
