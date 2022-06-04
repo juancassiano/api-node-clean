5da54b82dd0a47012289286ea8a0cee5708dd4f2
Juan Cassiano <juancassiano@hotmail.com>
2022-06-04 15:48:10 -0300

    :BUG connect to mongoDb before listen to app

--- src/main/index.js
+++ src/main/index.js
@@ -1,4 +1,11 @@
-const app = require("./config/app");
-app.listen(3333, () => {
-  console.log("server Running");
-});
+const MongoHelper = require("../infra/helpers/mongo-helper");
+const env = require("./config/env");
+
+MongoHelper.connect(env.mongoUrl)
+  .then(() => {
+    const app = require("./config/app");
+    app.listen(3333, () => {
+      console.log("server Running");
+    });
+  })
+  .catch(console.error);
